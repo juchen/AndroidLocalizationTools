a44e4c54c5f9d77383bf31a295ab02dec3f36917
Hsuan-Ju Henry Chen <hsuanju.ch@gmail.com>
2018-08-27 18:06:37 +0800

    Extracted the module MergeXlsxToXml

--- exp/exp2.hs
+++ exp/exp2.hs
@@ -10,6 +10,7 @@ import System.Directory
 import Text.XML.HXT.Arrow.XmlState.RunIOStateArrow
 import Data.Tree.NTree.TypeDefs
 import StringsFiles
+import MergeXlsxToXml
 
 parseFile' fname = runX $ (readDocument readConfig fname)
                           >>> (processBottomUp $ (isElem
@@ -22,113 +23,19 @@ parseFile' fname = runX $ (readDocument readConfig fname)
 copyXml fname = runX $ (readDocument [withValidate no] fname)
                         >>> (writeDocument [withIndent yes] "/tmp/out.xml")
 
-
-readConfig:: [SysConfig]
-readConfig = [withValidate no
-              , withRemoveWS no
-              , withPreserveComment yes
-              , withIgnoreNoneXmlContents no]
-
-writeConfig:: [SysConfig]
-writeConfig = [withIndent yes
-              , withRemoveWS no
-              , withPreserveComment yes]
-
-
-replaceText:: BigMap -> LangCode -> IOStateArrow BigMap XmlTree XmlTree
-replaceText bm lc = changeUserState removeUsed >>> isA (not.shouldRemove) >>> arr g
-  where
-    removeUsed:: XmlTree -> BigMap -> BigMap
-    removeUsed x bm = M.delete (keyXString $ xStringFromXmlTree x) bm
-    shouldRemove:: XmlTree -> Bool
-    shouldRemove x = case stringFromBigMap lc bm (keyXString $ xStringFromXmlTree x) of
-      Just Nothing -> True
-      _ -> False
-    g:: XmlTree -> XmlTree
-    g x = xmlTreeFromXString $ y'
-      where y = xStringFromXmlTree x
-            y' = case (stringFromBigMap lc bm (keyXString y)) of
-                  Just u -> y { text = unJust u }
-                  Nothing -> y
-
--- A fail trial.
-replaceText':: BigMap -> LangCode -> IOStateArrow BigMap XmlTree XmlTree
-replaceText' bm lc = changeUserState removeUsed >>> isA (not.shouldRemove)
-                     >>> (getName &&& returnA)
-                     >>> ((\f -> (arr $ \(k, x) -> head $ (runLA $ f k) x)) $ \k ->
-                             processBottomUp ((isText >>> (case stringFromBigMap lc bm k of
-                                          Just u -> txt (unJust u)
-                                          Nothing -> returnA)) `orElse` returnA))
-  where
-    removeUsed:: XmlTree -> BigMap -> BigMap
-    removeUsed x bm = M.delete (keyXString $ xStringFromXmlTree x) bm
-    shouldRemove:: XmlTree -> Bool
-    shouldRemove x = case stringFromBigMap lc bm (keyXString $ xStringFromXmlTree x) of
-      Just Nothing -> True
-      _ -> False
-
-processWithLangCode:: BigMap -> LangCode -> IOStateArrow BigMap XmlTree XmlTree
-processWithLangCode bm lc = processBottomUp $
-  ifA (isElem >>> hasName "string")
-      (ifA (hasAttrValue "translatable" (== "false"))
-         returnA
-         (replaceText bm lc))
-      returnA
--- processWithLangCode _ _ = processBottomUp $
---    (isA isStringElem) `orElse` returnA
---      where isStringElem (NTree (XTag s _) _) = ((show s) == "string")
---            isStringElem _ = False
-
-
-
 exp1 = do
   xmlFileName:_ <- getArgs
   bm <- bigMapFromFile xmlFileName
   restBm <- runXIOState (initialState bm) $ readDocument readConfig "/tmp/in.xml"
-                                              >>> processWithLangCode bm "values-ja-rJP"
+                                              >>> processTextReplacement bm "values-ja-rJP"
                                               >>> writeDocument writeConfig "/tmp/out.xml"
                                               >>> getUserState
   print $ length.(M.toList).head $ restBm
   return ()
 
-unJust (Just x) = x
-unJust Nothing = error "Bad situation"
-
-addRestStrings:: LangCode -> IOStateArrow s (BigMap, XmlTree) XmlTree
-addRestStrings lc = IOSLA $ (\s (bm, x) -> (runIOSLA (accBigMap bm lc)) s x)
-  where accBigMap:: BigMap -> LangCode -> IOStateArrow s XmlTree XmlTree
-        accBigMap bm lc = foldr cc returnA (fromBigMap bm lc)
-        fromContentMap:: LangCode -> (TextKey, ContentMap) -> Maybe (TextKey, TextContent)
-        fromContentMap lc (k, m) = do
-          v <- M.lookup lc m
-          return (k, v)
-        fromBigMap::BigMap -> LangCode ->[(TextKey, TextContent)]
-        fromBigMap bm lc = map unJust [c | c <- map (fromContentMap lc) (M.toList bm), c /= Nothing]
-        cc:: (TextKey, TextContent) -> IOStateArrow s XmlTree XmlTree -> IOStateArrow s XmlTree XmlTree
-        cc (k, c) a = a += (mkelem "string" [sattr "name" k] [txt c])
-
-stringsXmlArrow:: BigMap -> FilePath -> FilePath -> LangCode -> IOStateArrow BigMap XmlTree XmlTree
-stringsXmlArrow bm inFile outFile lc = readDocument readConfig inFile
-                            >>> processWithLangCode bm lc
-                            >>> processBottomUp ((isElem >>> hasName "resources"
-                                                  >>> (getUserState &&& returnA)
-                                                  >>> addRestStrings lc
-                                                  ) `orElse` returnA
-                                                )
-                            >>> writeDocument writeConfig outFile
-
-stringsXmlConversion:: BigMap -> FilePath -> FilePath -> LangCode -> IO [XmlTree]
-stringsXmlConversion  bm inFile outFile lc =
-  runXIOState (initialState bm) (stringsXmlArrow bm inFile outFile lc)
-
-renameFileArrow:: IOLA (FilePath, FilePath) (FilePath, FilePath)
-renameFileArrow = IOLA $ \(old, new) -> do
-  renameFile old new
-  return [(old, new)]
-
 exp2 = do
-  xmlFileName:_ <- getArgs
-  bm <- bigMapFromFile xmlFileName
+  xlsxFn:_ <- getArgs
+  bm <- bigMapFromFile xlsxFn
   restBm <- runXIOState (initialState bm) $ (stringsXmlArrow bm "/tmp/in.xml" "/tmp/out.xml" "values-zh-rCN")
                                             >>> getUserState
   print $ head restBm
@@ -136,19 +43,14 @@ exp2 = do
   return ()
 
 exp3 = do
-  xmlFileName:_ <- getArgs
-  bm <- bigMapFromFile xmlFileName
-  (\f -> runIOLA f undefined) $ (IOLA $ const valuesDirs)
-    >>> proc d -> do
-          old <- arr (++ "/strings.xml") -< d
-          new <- arr (++ "/strings.orig.xml") -< d
-          _ <- renameFileArrow -< (old, new)
-          (IOLA $ \(lc, inf, outf) -> stringsXmlConversion bm inf outf lc) -< (d, new, old)
+  xlsxFn:_ <- getArgs
+  bm <- bigMapFromFile xlsxFn
+  runIOLA ((IOLA (const valuesDirs)) >>> (mergeXlsxToXml bm)) undefined
   return ()
 
 exp4 = do
-  xmlFileName:_ <- getArgs
-  bm <- bigMapFromFile xmlFileName
+  xlsxFn:_ <- getArgs
+  bm <- bigMapFromFile xlsxFn
   stringsXmlConversion bm "/tmp/in.xml" "/tmp/out.xml" "values-zh-rCN"
 
 main = exp3
---
+++ src/MergeXlsxToXml.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE Arrows #-}
+module MergeXlsxToXml where
+
+import Text.XML.HXT.Core
+import FromStringsXmls
+import qualified Data.Map.Strict as M
+import Text.XML.HXT.Arrow.XmlState.RunIOStateArrow
+import System.Directory
+
+unJust (Just x) = x
+unJust Nothing = error "Bad situation"
+
+readConfig:: [SysConfig]
+readConfig = [withValidate no
+              , withRemoveWS no
+              , withPreserveComment yes
+              , withIgnoreNoneXmlContents no]
+
+writeConfig:: [SysConfig]
+writeConfig = [withIndent yes
+              , withRemoveWS no
+              , withPreserveComment yes]
+
+
+replaceText:: BigMap -> LangCode -> IOStateArrow BigMap XmlTree XmlTree
+replaceText bm lc = changeUserState removeUsed >>> isA (not.shouldRemove) >>> arr g
+  where
+    removeUsed:: XmlTree -> BigMap -> BigMap
+    removeUsed x bm = M.delete (keyXString $ xStringFromXmlTree x) bm
+    shouldRemove:: XmlTree -> Bool
+    shouldRemove x = case stringFromBigMap lc bm (keyXString $ xStringFromXmlTree x) of
+      Just Nothing -> True
+      _ -> False
+    g:: XmlTree -> XmlTree
+    g x = xmlTreeFromXString $ y'
+      where y = xStringFromXmlTree x
+            y' = case (stringFromBigMap lc bm (keyXString y)) of
+                  Just u -> y { text = unJust u }
+                  Nothing -> y
+
+processTextReplacement:: BigMap -> LangCode -> IOStateArrow BigMap XmlTree XmlTree
+processTextReplacement bm lc = processBottomUp $
+  ifA (isElem >>> hasName "string")
+      (ifA (hasAttrValue "translatable" (== "false"))
+         returnA
+         (replaceText bm lc))
+      returnA
+
+addRestStrings:: LangCode -> IOStateArrow s (BigMap, XmlTree) XmlTree
+addRestStrings lc = IOSLA $ (\s (bm, x) -> (runIOSLA (accBigMap bm lc)) s x)
+  where accBigMap:: BigMap -> LangCode -> IOStateArrow s XmlTree XmlTree
+        accBigMap bm lc = foldr cc returnA (fromBigMap bm lc)
+        fromContentMap:: LangCode -> (TextKey, ContentMap) -> Maybe (TextKey, TextContent)
+        fromContentMap lc (k, m) = do
+          v <- M.lookup lc m
+          return (k, v)
+        fromBigMap::BigMap -> LangCode ->[(TextKey, TextContent)]
+        fromBigMap bm lc = map unJust [c | c <- map (fromContentMap lc) (M.toList bm), c /= Nothing]
+        cc:: (TextKey, TextContent) -> IOStateArrow s XmlTree XmlTree -> IOStateArrow s XmlTree XmlTree
+        cc (k, c) a = a += (mkelem "string" [sattr "name" k] [txt c])
+
+processAddingText:: LangCode -> IOStateArrow BigMap XmlTree XmlTree
+processAddingText lc = processBottomUp ((isElem >>> hasName "resources"
+                                             >>> (getUserState &&& returnA)
+                                             >>> addRestStrings lc) `orElse` returnA)
+
+stringsXmlArrow:: BigMap -> FilePath -> FilePath -> LangCode -> IOStateArrow BigMap XmlTree XmlTree
+stringsXmlArrow bm inFile outFile lc = readDocument readConfig inFile
+                            >>> processTextReplacement bm lc
+                            >>> processAddingText lc
+                            >>> writeDocument writeConfig outFile
+
+stringsXmlConversion:: BigMap -> FilePath -> FilePath -> LangCode -> IO [XmlTree]
+stringsXmlConversion  bm inFile outFile lc =
+  runXIOState (initialState bm) (stringsXmlArrow bm inFile outFile lc)
+
+renameFileArrow:: IOLA (FilePath, FilePath) (FilePath, FilePath)
+renameFileArrow = IOLA $ \(old, new) -> do
+  renameFile old new
+  return [(old, new)]
+
+mergeXlsxToXml:: BigMap -> IOLA FilePath XmlTree
+mergeXlsxToXml bm = proc d -> do
+          old <- arr (++ "/strings.xml") -< d
+          new <- arr (++ "/strings.orig.xml") -< d
+          bak <- arr (++ "/strings.xml.bak") -< d
+          _ <- renameFileArrow -< (old, new)
+          x <- (IOLA $ \(lc, inf, outf) -> stringsXmlConversion bm inf outf lc) -< (d, new, old)
+          _ <- renameFileArrow -< (new, bak)
+          returnA -< x
+
+-- A fail trial.
+replaceText':: BigMap -> LangCode -> IOStateArrow BigMap XmlTree XmlTree
+replaceText' bm lc = changeUserState removeUsed >>> isA (not.shouldRemove)
+                     >>> (getName &&& returnA)
+                     >>> ((\f -> (arr $ \(k, x) -> head $ (runLA $ f k) x)) $ \k ->
+                             processBottomUp ((isText >>> (case stringFromBigMap lc bm k of
+                                          Just u -> txt (unJust u)
+                                          Nothing -> returnA)) `orElse` returnA))
+  where
+    removeUsed:: XmlTree -> BigMap -> BigMap
+    removeUsed x bm = M.delete (keyXString $ xStringFromXmlTree x) bm
+    shouldRemove:: XmlTree -> Bool
+    shouldRemove x = case stringFromBigMap lc bm (keyXString $ xStringFromXmlTree x) of
+      Just Nothing -> True
+      _ -> False
