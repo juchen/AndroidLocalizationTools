544d01e1ead2051e574c4528bc70b29f352d1aa7
Henry Hsuan-Ju Chen <hsuanju.ch@gmail.com>
2018-10-24 11:12:48 +0800

    Remove redundancy.

--- app/to_xlsx.hs
+++ app/to_xlsx.hs
@@ -34,9 +34,6 @@ pathsToXlsx paths = do
     xlsx = def & atSheet (T.pack "Localization") ?~ sheet
   return xlsx
   where
-    -- A tab separated CSV is used because the can be comma in some strings.
-    f4:: (TextKey, [TextContent]) -> String
-    f4 (k, l) = foldl (\b a -> b ++ "\t" ++ a ++ "") k l
     f3:: [LangCode] -> BigMap -> [(TextKey, [TextContent])]
     f3 l bm = map (\(x1, x2) -> (x1, (f l x2))) (f2 bm)
     f2:: BigMap -> [(TextKey, ContentMap)]
